9027e78cbc61fbc0acf27d74fd1fb073ea16f518
Joshua Li <joshuarli98@gmail.com>
2023-08-28 14:54:15 -0700

    pin gha as an example

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import os
 from typing import Sequence
 
-from devenv import doctor
+from devenv import doctor, pin_gha
 from devenv.lib import gitroot
 
 
@@ -12,7 +12,8 @@ class CustomArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         print(
             f"""commands:
-doctor - {doctor.help}
+doctor  - {doctor.help}
+pin-gha - {pin_gha.help}
 """
         )
         raise SystemExit(1)
@@ -25,11 +26,14 @@ def main(argv: Sequence[str] | None = None) -> int:
         "command",
         choices={
             "doctor",
+            "pin-gha",
         },
     )
     args, remainder = parser.parse_known_args(argv)
 
     # self_update()
+
+    os.chdir(args.pwd)
 
     # TODO: read a well-known json for preferences
     coderoot = "dev"
@@ -50,6 +54,8 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     if args.command == "doctor":
         return doctor.main(context, remainder)
+    elif args.command == "pin-gha":
+        return pin_gha.main(remainder)
 
     return 0
 
