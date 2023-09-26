e9e40fd49113f88ba07fdd2a0a91deb48c3d512d
Joshua Li <joshuarli98@gmail.com>
2023-09-26 11:37:19 -0700

    print

--- lib/github.py
+++ lib/github.py
@@ -18,6 +18,7 @@ def check_ssh_access() -> bool:
     except RuntimeError as e:
         # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/testing-your-ssh-connection
         if "You've successfully authenticated" not in f"{e}":
+            print(f"{e}")
             return False
     try:
         with tempfile.TemporaryDirectory() as tmpdir:
