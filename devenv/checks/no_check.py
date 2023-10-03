b38866ec58af4325190b89f76bb344b914c54110
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-10-03 14:09:13 -0700

    linting

--- devenv/checks/no_check.py
+++ devenv/checks/no_check.py
@@ -1,13 +1,6 @@
 from __future__ import annotations
 
-import os
 from typing import Set
-from typing import Tuple
-
-from devenv.lib import fs
-from devenv.lib import proc
-from devenv.lib_check.types import checker
-from devenv.lib_check.types import fixer
 
 tags: Set[str] = set()
 name = "no check"
