b5ad4e6e7af9fa18f6d7bba79a7de8141dc03399
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-10-05 11:54:55 -0700

    Making having a fix optional

--- tests/doctor/test_run_checks.py
+++ tests/doctor/test_run_checks.py
@@ -5,7 +5,9 @@ from concurrent.futures import ThreadPoolExecutor
 from devenv import doctor
 from devenv.tests.doctor.devenv.checks import failing_check
 from devenv.tests.doctor.devenv.checks import failing_check_with_msg
+from devenv.tests.doctor.devenv.checks import failing_check_with_no_fix
 from devenv.tests.doctor.devenv.checks import passing_check
+from devenv.tests.doctor.devenv.checks import passing_check_with_no_fix
 
 
 def test_run_checks_no_checks() -> None:
@@ -46,3 +48,23 @@ def test_run_checks_skip(capsys) -> None:  # type: ignore
     ) == {first_check: (True, "")}
     captured = capsys.readouterr()
     assert captured.out == "    ⏭️  Skipped failing check\n"
+
+
+def test_run_multiple_passing_checks() -> None:
+    first_check = doctor.Check(passing_check)
+    second_check = doctor.Check(passing_check_with_no_fix)
+    assert doctor.run_checks([first_check, second_check], ThreadPoolExecutor()) == {
+        first_check: (True, ""),
+        second_check: (True, ""),
+    }
+
+
+def test_run_multiple_failing_checks() -> None:
+    first_check = doctor.Check(failing_check)
+    second_check = doctor.Check(failing_check_with_msg)
+    third_check = doctor.Check(failing_check_with_no_fix)
+    assert doctor.run_checks([first_check, second_check, third_check], ThreadPoolExecutor()) == {
+        first_check: (False, ""),
+        second_check: (False, "check failed"),
+        third_check: (False, ""),
+    }
