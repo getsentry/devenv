c22c45ddca5aafc8e578cd542b8a264fb41823f5
Joshua Li <joshuarli98@gmail.com>
2023-11-02 13:09:26 -0700

    fix ci

--- ci/devenv-bootstrap.sh
+++ ci/devenv-bootstrap.sh
@@ -5,29 +5,18 @@ set -euxo pipefail
 # we don't have permissions to clone getsentry which is a good thing
 # eventually we should move this bootstrap testing over to getsentry repo
 # in the meantime, mock it so that pip install -e has something to chew on
-mkdir -p "$HOME/repo/getsentry"
-cat <<EOF > "$HOME/repo/getsentry/pyproject.toml"
+mkdir -p "$HOME/code/getsentry"
+cat <<EOF > "$HOME/code/getsentry/pyproject.toml"
 [project]
 name = "getsentry-mock"
 version = "0.0.0"
 EOF
 
 : PATH: "$PATH"
-cat ~/.bashrc || : just looking
 
 # note: colima will be used and is necessary for a docker runtime on
 #       macos GitHub runners
-ls -l \
-  "$HOME/repo/getsentry/.venv/bin/activate" \
-  "$HOME/.local/share/sentry-devenv/virtualenvs/sentry" \
-|| true
 devenv bootstrap || true
 
-ls -l \
-  "$HOME/repo/getsentry/.venv/bin/activate" \
-  "$HOME/.local/share/sentry-devenv/virtualenvs/sentry" \
-|| true
-
-
-cd "$HOME/repo/sentry"
+cd "$HOME/code/sentry"
 direnv allow
