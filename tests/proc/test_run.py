5664964c6737fc1ad983d4b34bc9b53352dcd6a9
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-12-01 16:03:31 -0800

    Refactor test_run.py

--- tests/proc/test_run.py
+++ tests/proc/test_run.py
@@ -1,82 +1,67 @@
 from __future__ import annotations
 
-from subprocess import CalledProcessError
+import os
 from unittest.mock import patch
 
 import pytest
 
-from devenv.lib.proc import base_env
 from devenv.lib.proc import run
 
 
 def test_run_with_stdout() -> None:
     cmd = ("echo", "Hello, World!")
     expected_output = "Hello, World!"
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        mock_run.return_value.stdout = expected_output.encode()
-        result = run(cmd, stdout=True)
+    result = run(cmd, stdout=True)
 
     assert result == expected_output
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=-1, cwd=None, env=env
-    )
 
 
 def test_run_without_stdout() -> None:
     cmd = ("echo", "Hello, World!")
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        result = run(cmd)
+    result = run(cmd)
 
     assert result is None
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=None, cwd=None, env=env
-    )
 
 
-def test_run_with_pathprepend() -> None:
+def test_run_with_debug() -> None:
     cmd = ("echo", "Hello, World!")
-    pathprepend = "/path/to/bin"
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
-    env["PATH"] = f"{pathprepend}:{env['PATH']}"
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        mock_run.return_value.stdout = None
-        result = run(cmd, pathprepend=pathprepend)
+    with patch("devenv.lib.proc.constants.DEBUG", True), patch(
+        "devenv.lib.proc.xtrace"
+    ) as mock_xtrace:
+        run(cmd)
 
-    assert result is None
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=None, cwd=None, env=env
-    )
+    mock_xtrace.assert_called_once_with(cmd)
+
+
+def test_run_with_pathprepend(tmp_path: str) -> None:
+    dummy_executable = os.path.join(tmp_path, "dummy_executable")
+    with open(dummy_executable, "w") as f:
+        f.write("#!/bin/sh\necho Hello, World!")
+    os.chmod(dummy_executable, 0o755)
+
+    cmd = ("dummy_executable",)
+    pathprepend = tmp_path
+
+    result = run(cmd, pathprepend=pathprepend, stdout=True)
+
+    assert result == "Hello, World!"
 
 
 def test_run_command_not_found() -> None:
     cmd = ("invalid_command",)
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        mock_run.side_effect = FileNotFoundError("Command not found")
-        with pytest.raises(SystemExit):
-            run(cmd, exit=True)
+    with pytest.raises(RuntimeError):
+        run(cmd, exit=False)
 
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=None, cwd=None, env=env
-    )
+
+def test_run_command_not_found_with_exit() -> None:
+    cmd = ("invalid_command",)
+
+    with pytest.raises(SystemExit):
+        run(cmd, exit=True)
 
 
 def test_run_with_custom_env() -> None:
@@ -88,36 +73,27 @@ def test_run_with_custom_env() -> None:
     assert result == "value1\nvalue2"
 
 
-def test_run_with_cwd() -> None:
-    cmd = ("echo", "Hello, World!")
-    cwd = "/path/to/directory"
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
+def test_run_with_cwd(tmp_path: str) -> None:
+    text = "Hello, World!"
+    with open(os.path.join(tmp_path, "test.txt"), "w") as f:
+        f.write(text)
+    cmd = ("cat", "test.txt")
+    cwd = tmp_path
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        mock_run.return_value.stdout = None
-        result = run(cmd, cwd=cwd)
+    result = run(cmd, cwd=cwd, stdout=True)
 
-    assert result is None
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=None, cwd=cwd, env=env
-    )
+    assert result == text
 
 
 def test_run_command_failed() -> None:
     cmd = ("ls", "nonexistent_directory")
-    user_environ = {"PATH": "/usr/local/bin:/usr/bin"}
-    env = {**user_environ, **base_env}
 
-    with patch("devenv.lib.proc.subprocess_run") as mock_run, patch(
-        "devenv.lib.proc.constants.user_environ", user_environ
-    ):
-        mock_run.side_effect = CalledProcessError(1, cmd)
-        with pytest.raises(SystemExit):
-            run(cmd, exit=True)
+    with pytest.raises(RuntimeError):
+        run(cmd, exit=False)
 
-    mock_run.assert_called_once_with(
-        cmd, check=True, stdout=None, cwd=None, env=env
-    )
+
+def test_run_command_failed_with_exit() -> None:
+    cmd = ("ls", "nonexistent_directory")
+
+    with pytest.raises(SystemExit):
+        run(cmd, exit=True)
