24ac294484b982889e09a8d249c7220536d41ce7
Joshua Li <joshuarli98@gmail.com>
2023-12-03 18:23:49 -0800

    use tmp_path

--- tests/lib/test_fs.py
+++ tests/lib/test_fs.py
@@ -2,21 +2,16 @@ from __future__ import annotations
 
 import contextlib
 import os
+import pathlib
 import subprocess
 from collections.abc import Generator
 
 from devenv.lib.fs import gitroot
 
-_gitroot = (
-    subprocess.run(("git", "rev-parse", "--show-toplevel"), capture_output=True)
-    .stdout.decode()
-    .strip()
-)
-
 
 # TODO: replace with contextlib.chdir when we can use python3.11
 @contextlib.contextmanager
-def chdir(d: str) -> Generator[None, None, None]:
+def chdir(d: str | pathlib.Path) -> Generator[None, None, None]:
     curdir = os.getcwd()
     try:
         os.chdir(d)
@@ -25,12 +20,38 @@ def chdir(d: str) -> Generator[None, None, None]:
         os.chdir(curdir)
 
 
-def test_gitroot() -> None:
-    assert gitroot() == _gitroot
-    assert os.path.isdir(f"{gitroot()}/.git")
+def test_gitroot(tmp_path: pathlib.Path) -> None:
+    with chdir(tmp_path):
+        subprocess.run(("git", "init"))
+
+        _gitroot = (
+            subprocess.run(
+                ("git", "rev-parse", "--show-toplevel"), capture_output=True
+            )
+            .stdout.decode()
+            .strip()
+        )
+
+        assert gitroot() == _gitroot
+        assert os.path.isdir(f"{gitroot()}/.git")
 
 
-def test_gitroot_cd() -> None:
-    with chdir(f"{_gitroot}/tests"):
+def test_gitroot_cd(tmp_path: pathlib.Path) -> None:
+    with chdir(tmp_path):
+        subprocess.run(("git", "init"))
+
+    (tmp_path / "nested").mkdir()
+
+    with chdir(f"{tmp_path}/nested"):
+        subprocess.run(("git", "init"))
+
+        _gitroot = (
+            subprocess.run(
+                ("git", "rev-parse", "--show-toplevel"), capture_output=True
+            )
+            .stdout.decode()
+            .strip()
+        )
+
         assert gitroot() == _gitroot
         assert os.path.isdir(f"{gitroot()}/.git")
