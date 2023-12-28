38d57e40ee384972495e88d9b91e94658ae19270
Joshua Li <joshuarli98@gmail.com>
2023-12-28 13:02:12 -0800

    atomically move to dest only after successful complete download to tempfile

--- devenv/lib/archive.py
+++ devenv/lib/archive.py
@@ -5,6 +5,7 @@ import os
 import secrets
 import shutil
 import tarfile
+import tempfile
 import urllib.request
 from urllib.error import HTTPError
 
@@ -21,23 +22,24 @@ def download(url: str, sha256: str, dest: str = "") -> str:
             resp = urllib.request.urlopen(url)
         except HTTPError as e:
             raise RuntimeError(f"Error getting {url}: {e}")
-        with open(dest, "wb") as f:
-            shutil.copyfileobj(resp, f)
 
-    checksum = hashlib.sha256()
-    with open(dest, mode="rb") as f:
-        bts = f.read(4096)
-        while bts:
-            checksum.update(bts)
-            bts = f.read(4096)
+        with tempfile.NamedTemporaryFile(delete=False) as tmpf:
+            shutil.copyfileobj(resp, tmpf)
+            tmpf.seek(0)
+            checksum = hashlib.sha256()
+            buf = tmpf.read(4096)
+            while buf:
+                checksum.update(buf)
+                buf = tmpf.read(4096)
 
-    if not secrets.compare_digest(checksum.hexdigest(), sha256):
-        os.remove(dest)
-        raise RuntimeError(
-            f"checksum mismatch for {url}:\n"
-            f"- got: {checksum.hexdigest()}\n"
-            f"- expected: {sha256}\n"
-        )
+            if not secrets.compare_digest(checksum.hexdigest(), sha256):
+                raise RuntimeError(
+                    f"checksum mismatch for {url}:\n"
+                    f"- got: {checksum.hexdigest()}\n"
+                    f"- expected: {sha256}\n"
+                )
+
+            os.replace(tmpf.name, dest)
 
     return dest
 
