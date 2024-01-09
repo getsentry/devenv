d4056da42f8fa4587da44a1678189ec02d7b8f66
Joshua Li <joshuarli98@gmail.com>
2024-01-09 14:11:24 -0800

    remove this test since other tests basically cover it

--- tests/lib/test_volta.py
+++ tests/lib/test_volta.py
@@ -8,7 +8,6 @@ import pytest
 
 from devenv.lib.volta import _sha256
 from devenv.lib.volta import _version
-from devenv.lib.volta import download_and_unpack_archive
 from devenv.lib.volta import install
 from devenv.lib.volta import install_volta
 from devenv.lib.volta import populate_volta_home_with_shims
@@ -129,34 +128,6 @@ def test_install_volta_unexpected_platform() -> None:
     with patch("platform.system", return_value="Unexpected"):
         with pytest.raises(UnexpectedPlatformError):
             install_volta("/path/to/unpack")
-
-
-def test_download_and_unpack_archive() -> None:
-    with patch("platform.system", return_value="Darwin"), patch(
-        "platform.machine", return_value="x86_64"
-    ), patch(
-        "devenv.lib.volta.archive.download",
-        return_value="/path/to/archive_file",
-    ) as mock_download, patch(
-        "devenv.lib.volta.archive.unpack"
-    ) as mock_unpack:
-        name = f"volta-{_version}-macos.tar.gz"
-        unpack_into = "/path/to/unpack"
-
-        # Call the function under test
-        download_and_unpack_archive(name, unpack_into)
-
-        # Assert that the mocked functions were called with the expected arguments
-        mock_download.assert_called_once_with(
-            (
-                "https://github.com/volta-cli/volta/releases/download/"
-                f"v{_version}/volta-{_version}-macos.tar.gz"
-            ),
-            _sha256[name],
-        )
-        mock_unpack.assert_called_once_with(
-            "/path/to/archive_file", unpack_into
-        )
 
 
 def test_populate_volta_home_with_shims() -> None:
