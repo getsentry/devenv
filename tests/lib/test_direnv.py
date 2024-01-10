c9a091d6e5363162340aa93ea447dd5fb0f19de7
Joshua Li <joshuarli98@gmail.com>
2024-01-10 13:23:15 -0800

    lolfix

--- tests/lib/test_direnv.py
+++ tests/lib/test_direnv.py
@@ -11,6 +11,7 @@ def test_install() -> None:
     with mock.patch.dict(os.environ, {"SHELL": "/bin/bash"}):
         # need to import devenv first here so that SHELL can take effect
         from devenv.constants import bin_root
+        from devenv.constants import home
 
     from devenv.lib.direnv import install
     from devenv.lib.direnv import _version
@@ -27,7 +28,7 @@ def test_install() -> None:
     ) as mock_proc_run:
         install()
         mock_lib_fs_idempotent_add.assert_has_calls(
-            [call("/Users/josh/.bashrc", '\neval "$(direnv hook bash)"\n')]
+            [call(f"{home}/.bashrc", '\neval "$(direnv hook bash)"\n')]
         )
         mock_archive_download.assert_has_calls(
             [
