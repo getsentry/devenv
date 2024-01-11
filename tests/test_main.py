625a3325ba3230bd2019cf53cbaad31d9464f278
Joshua Li <joshuarli98@gmail.com>
2024-01-11 15:06:59 -0800

    done

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+import subprocess
 from unittest.mock import call
 from unittest.mock import patch
 
@@ -10,15 +12,14 @@ from tests.utils import chdir
 def test(tmp_path: str) -> None:
     coderoot = f"{tmp_path}/coderoot"
     configroot = f"{tmp_path}/configroot"
+
     with patch("devenv.main.CI", True), patch("sentry_sdk.init"), patch(
         "devenv.main.config_root", configroot
-    ), patch("devenv.bootstrap.main") as mock_bootstrap:
+    ), patch("devenv.bootstrap.main") as mock_bootstrap, patch(
+        "devenv.sync.main"
+    ) as mock_sync:
         main.DEFAULT_CONFIG["devenv"]["coderoot"] = coderoot
         main.devenv(("/path/to/argv0", "bootstrap"))
-        # assert mock_makedirs.mock_calls == [
-        #    call(configroot, exist_ok=True),
-        #    call(coderoot, exist_ok=True),
-        # ]
         with open(f"{configroot}/config.ini", "r") as f:
             assert (
                 f.read()
@@ -28,10 +29,22 @@ coderoot = {coderoot}
 
 """
             )
-        assert mock_bootstrap.mock_calls == [call(coderoot, [])]
+            assert mock_bootstrap.mock_calls == [call(coderoot, [])]
 
-        # with our config written, let's call sync
-        with chdir(coderoot):
+        # with our config written, let's mock a repo then call sync
+        repo = "repo"
+        reporoot = f"{coderoot}/repo"
+        os.makedirs(reporoot)
+        subprocess.run(("git", "init", "--quiet", "--bare", reporoot))
+        with chdir(reporoot), patch(
+            "os.makedirs", wraps=os.makedirs
+        ) as os_makedirs:
             main.devenv(("/path/to/argv0", "sync"))
 
-        # this time, we should have early returned from initialize_config
+            # this time, we should have early returned from initialize_config
+            # so only the os.makedirs call to coderoot should have happened
+            # otherwise we would also have a call(configroot, exist_ok=True)
+            assert os_makedirs.mock_calls == [call(coderoot, exist_ok=True)]
+            assert mock_sync.mock_calls == [
+                call({"repo": repo, "reporoot": reporoot}, [])
+            ]
