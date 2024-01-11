94206d3ba8e04dfe42fe63944b8095e377a67967
Joshua Li <joshuarli98@gmail.com>
2024-01-11 14:09:18 -0800

    test: direnv (#64)

---
+++ tests/lib/test_direnv.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import os
+from unittest import mock
+from unittest.mock import call
+from unittest.mock import patch
+
+
+# imo this is enough coverage (as in just keeping it to bash + darwin-arm64)
+# we just want to verify that the important functions were called like fs_idempotent_add
+def test_install() -> None:
+    with mock.patch.dict(os.environ, {"SHELL": "/bin/bash"}):
+        # need to import devenv first here so that SHELL can take effect
+        from devenv.constants import bin_root
+        from devenv.constants import home
+
+    from devenv.lib.direnv import install
+    from devenv.lib.direnv import _version
+    from devenv.lib.direnv import _sha256
+
+    with patch("devenv.lib.direnv.MACHINE", "arm64"), patch(
+        "devenv.lib.direnv.sys.platform", "darwin"
+    ), patch("devenv.lib.archive.download") as mock_archive_download, patch(
+        "devenv.lib.fs.idempotent_add"
+    ) as mock_lib_fs_idempotent_add, patch(
+        "os.chmod"
+    ), patch(
+        "devenv.lib.direnv.proc.run", side_effect=[_version]  # direnv version
+    ) as mock_proc_run:
+        install()
+        assert mock_lib_fs_idempotent_add.mock_calls == [
+            call(f"{home}/.bashrc", '\neval "$(direnv hook bash)"\n')
+        ]
+        assert mock_archive_download.mock_calls == [
+            call(
+                f"https://github.com/direnv/direnv/releases/download/v{_version}/direnv.darwin-arm64",
+                _sha256["direnv.darwin-arm64"],
+                dest=f"{bin_root}/direnv",
+            )
+        ]
+        assert mock_proc_run.mock_calls == [
+            call((f"{bin_root}/direnv", "version"), stdout=True)
+        ]
