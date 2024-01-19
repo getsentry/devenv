4e818a1ce45135ae00b3b7c0a8f8d0e0c003fb6e
Joshua Li <joshuarli98@gmail.com>
2024-01-19 14:59:56 -0800

    [skip ci] wip

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -1,15 +1,46 @@
 from __future__ import annotations
 
 import os
+import subprocess
 from unittest.mock import call
 from unittest.mock import patch
 
-from devenv import sync
-from devenv.constants import pythons_root
+config_template = """
+[devenv]
+coderoot = {coderoot}
+"""
 
-# TODO: devenv exec -- env and devenv exec env
-# lets also have a test for .venv/bin/foo
 
+def test_python_project(tmp_path: str) -> None:
+    home = tmp_path
+    coderoot = f"{home}/coderoot"
 
-def test(tmp_path: str) -> None:
-    pass
+    config_dir = f"{home}/.config/sentry-devenv"
+    os.makedirs(config_dir)
+    with open(f"{config_dir}/config.ini", "w") as f:
+        f.write(config_template.format(coderoot=coderoot))
+
+    reporoot = f"{coderoot}/repo"
+    os.makedirs(f"{reporoot}/.venv")
+    with open(f"{reporoot}/config.ini", "w") as f:
+        f.write(
+            """
+[python]
+version = 3.10.3
+"""
+        )
+
+    # first, let's test an outdated venv
+    with open(f"{reporoot}/.venv/pyvenv.cfg", "w") as f:
+        f.write("version = 3.8.16")
+
+    env = {**os.environ, "HOME": home}
+    p = subprocess.run(
+        ("devenv", "exec", "env"), cwd=coderoot, env=env, capture_output=True
+    )
+    assert p.returncode == 0
+
+    # now we should modify cfg and it should amtch
+
+    # devenv exec -- env should also work (argparse)
+    # lets also have a test for .venv/bin/foo
