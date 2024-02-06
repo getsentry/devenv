1876009e08f96a966ae660d5e7d3bd8c09e480c2
Joshua Li <joshuarli98@gmail.com>
2024-02-05 20:36:24 -0800

    wip

--- devenv/lib/venv.py
+++ devenv/lib/venv.py
@@ -5,8 +5,10 @@ import shutil
 from typing import Optional
 
 from devenv import pythons
+from devenv.constants import bin_root
 from devenv.constants import venvs_root
 from devenv.lib import config
+from devenv.lib import fs
 from devenv.lib import proc
 
 VENV_OK = 1
@@ -15,7 +17,7 @@ VENV_NOT_PRESENT = 3
 VENV_NOT_CONFIGURED = 4
 
 
-# example venv configuration section.
+# example venv configuration section:
 #
 # [venv.sentry-kube]
 # python = 3.11.6
@@ -24,17 +26,26 @@ VENV_NOT_CONFIGURED = 4
 # editable =
 #   k8s/cli
 #   k8s/cli/libsentrykube
+# bins =
+#   sentry-kube
+#   sentry-kube-pop
 #
 # [venv.salt]
 # python = 3.11.6
-# ...
+# requirements = salt/requirements.txt
+# bins =
+#   salt-ssh
+#   ...
 #
-# venv_dir, python_version, requirements, editable_paths = get(reporoot, "sentry-kube")
+# example usage:
+#
+# venv_dir, python_version, requirements, editable_paths, bins = get(reporoot, "sentry-kube")
 # url, sha256 = config.get_python(reporoot, python_version)
 # ensure(path, python_version, url, sha256)
+# sync(venv_dir, requirements, editable_paths, bins)
 def get(
     reporoot: str, name: str
-) -> tuple[str, str, str, Optional[tuple[str, ...]]]:
+) -> tuple[str, str, str, Optional[tuple[str, ...]], Optional[tuple[str, ...]]]:
     cfg = config.get_repo(reporoot)
 
     if not cfg.has_section(f"venv.{name}"):
@@ -49,11 +60,18 @@ def get(
             f"{reporoot}/{path}" for path in editable_paths.strip().split("\n")
         )
 
+    bins = venv.get("bins", None)
+    if bins is not None:
+        bins = tuple(
+            f"{venv_dir}/bin/{name}" for name in bins.strip().split("\n")
+        )
+
     return (
         venv_dir,
         venv["python"],
         f"{reporoot}/{venv['requirements']}",
         editable_paths,
+        bins,
     )
 
 
@@ -61,6 +79,7 @@ def sync(
     venv_dir: str,
     requirements: str,
     editable_paths: Optional[tuple[str, ...]] = None,
+    bins: Optional[tuple[str, ...]] = None,
 ) -> None:
     cmd: tuple[str, ...] = (
         f"{venv_dir}/bin/python",
@@ -78,6 +97,12 @@ def sync(
         for path in editable_paths:
             cmd = (*cmd, "-e", path)
     proc.run(cmd)
+
+    if bins is not None:
+        for name in bins:
+            fs.ensure_symlink(
+                expected_src=f"{venv_dir}/bin/{name}", dest=f"{bin_root}/{name}"
+            )
 
 
 # legacy, used for sentry/getsentry
