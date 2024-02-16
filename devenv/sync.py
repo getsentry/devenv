89b3873dca1a30b3cf58933a62853ef4ca7c535b
Joshua Li <joshuarli98@gmail.com>
2024-02-16 13:20:16 -0800

    pin colima to 0.6.2 for ci

--- devenv/sync.py
+++ devenv/sync.py
@@ -11,6 +11,7 @@ from typing import Tuple
 
 from devenv import constants
 from devenv import pythons
+from devenv.constants import CI
 from devenv.constants import DARWIN
 from devenv.constants import home
 from devenv.constants import MACHINE
@@ -149,11 +150,20 @@ def main(context: Dict[str, str], argv: Sequence[str] | None = None) -> int:
 
     if DARWIN:
         # we don't officially support colima on linux yet
-        colima.install(
-            repo_config["colima"]["version"],
-            repo_config["colima"][SYSTEM_MACHINE],
-            repo_config["colima"][f"{SYSTEM_MACHINE}_sha256"],
-        )
+        if CI:
+            # colima 0.6.8 doesn't work with macos-13,
+            # but integration coverage is still handy
+            colima.install(
+                "0.6.2",
+                "darwin_x86_64",
+                "43ef3fc80a8347d51b8ec1706f9caf8863bd8727a6f7532caf1ccd20497d8485",
+            )
+        else:
+            colima.install(
+                repo_config["colima"]["version"],
+                repo_config["colima"][SYSTEM_MACHINE],
+                repo_config["colima"][f"{SYSTEM_MACHINE}_sha256"],
+            )
 
         # TODO: move limactl version into per-repo config
         limactl.install()
