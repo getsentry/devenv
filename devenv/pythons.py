b0b36538c28694f12042271850312aa46e530207
Joshua Li <joshuarli98@gmail.com>
2024-03-14 14:02:04 -0700

    remaining except bin_root

--- devenv/pythons.py
+++ devenv/pythons.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 import os
 
-from devenv.constants import pythons_root
+from devenv.constants import home
 from devenv.lib import archive
 
 
 def get(python_version: str, url: str, sha256: str) -> str:
-    unpack_into = f"{pythons_root}/{python_version}"
+    unpack_into = f"{home}/pythons/{python_version}"
 
     if os.path.exists(f"{unpack_into}/python/bin/python3"):
         return f"{unpack_into}/python/bin/python3"
