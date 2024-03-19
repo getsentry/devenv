df0dd48f21148470f99b7efaccbfee9a4b03656f
Joshua Li <joshuarli98@gmail.com>
2024-03-19 12:54:48 -0700

    colima

--- devenv/lib/colima.py
+++ devenv/lib/colima.py
@@ -4,20 +4,20 @@ import os
 import shutil
 import tempfile
 
-from devenv.constants import bin_root
 from devenv.constants import home
 from devenv.lib import archive
 from devenv.lib import proc
 
 
 def _install(url: str, sha256: str, into: str) -> None:
+    os.makedirs(into, exist_ok=True)
     with tempfile.TemporaryDirectory(dir=into) as tmpd:
         archive.download(url, sha256, dest=f"{tmpd}/colima")
         os.replace(f"{tmpd}/colima", f"{into}/colima")
         os.chmod(f"{into}/colima", 0o775)
 
 
-def uninstall() -> None:
+def uninstall(bin_root: str) -> None:
     for d in (f"{home}/.lima",):
         shutil.rmtree(d, ignore_errors=True)
 
@@ -26,7 +26,7 @@ def uninstall() -> None:
             os.remove(f)
 
 
-def install(version: str, url: str, sha256: str) -> None:
+def install(version: str, url: str, sha256: str, bin_root: str) -> None:
     if shutil.which("colima", path=bin_root) == f"{bin_root}/colima":
         stdout = proc.run((f"{bin_root}/colima", "--version"), stdout=True)
         installed_version = stdout.strip().split()[-1]
@@ -35,7 +35,7 @@ def install(version: str, url: str, sha256: str) -> None:
         print(f"installed colima {installed_version} is outdated!")
 
     print(f"installing colima {version}...")
-    uninstall()
+    uninstall(bin_root)
     _install(url, sha256, bin_root)
 
     stdout = proc.run((f"{bin_root}/colima", "--version"), stdout=True)
