8b3d376583f141b57e3b20bc0127c74cb96380d5
Dan Bennett <dan.bennett@sentry.io>
2024-04-12 17:36:59 -0700

    Added some sloppiness to sentry and ops repo selection

--- devenv/fetch.py
+++ devenv/fetch.py
@@ -16,7 +16,77 @@ help = "Fetches a respository"
 ExitCode: TypeAlias = "str | int | None"
 
 
-def _fetch(
+def main(coderoot: str, argv: Sequence[str] | None = None) -> ExitCode:
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument(
+        "repo",
+        type=str,
+        nargs="?",
+        default="sentry",
+        help="the repository to fetch e.g., getsentry/sentry",
+    )
+
+    args = parser.parse_args(argv)
+
+    if args.repo in ["ops", "getsentry/ops"]:
+        fetch(coderoot, "getsentry/ops")
+        fetch(coderoot, "getsentry/terraform-modules", sync=False)
+
+        print(
+            f"""
+    All done! Please close this terminal window and start a fresh one.
+
+    ops repo is at: {coderoot}/ops
+    """
+        )
+    elif args.repo in [
+        "sentry",
+        "getsentry",
+        "getsentry/sentry",
+        "getsentry/getsentry",
+    ]:
+        # git@ clones forces the use of cloning through SSH which is what we want,
+        # though CI must clone open source repos via https (no git authentication)
+        fetch(coderoot, "getsentry/sentry", auth=CI is None, sync=False)
+
+        print("Installing sentry's brew dependencies...")
+        if CI:
+            if DARWIN:
+                # Installing everything from brew takes too much time,
+                # and chromedriver cask flakes occasionally. Really all we need to
+                # set up the devenv is colima and docker-cli.
+                # This is also required for arm64 macOS GHA runners.
+                # We manage colima, so just need to install docker + qemu here.
+                proc.run(("brew", "install", "docker", "qemu"))
+        else:
+            proc.run(
+                (f"{homebrew_bin}/brew", "bundle"), cwd=f"{coderoot}/sentry"
+            )
+
+        proc.run(
+            (sys.executable, "-P", "-m", "devenv", "sync"),
+            cwd=f"{coderoot}/sentry",
+        )
+
+        if not CI and not EXTERNAL_CONTRIBUTOR:
+            fetch(coderoot, "getsentry/getsentry")
+
+        print(
+            f"""
+    All done! Please close this terminal window and start a fresh one.
+
+    Sentry has been set up in {coderoot}/sentry. cd into it and you should
+    be able to run `sentry devserver`.
+    """
+        )
+
+    else:
+        fetch(coderoot, args.repo)
+
+    return 0
+
+
+def fetch(
     coderoot: str, repo: str, auth: bool = True, sync: bool = True
 ) -> None:
     org, slug = repo.split("/")
@@ -55,61 +125,3 @@ def _fetch(
 
     if sync:
         proc.run((sys.executable, "-P", "-m", "devenv", "sync"), cwd=codepath)
-
-
-def main(coderoot: str, argv: Sequence[str] | None = None) -> ExitCode:
-    parser = argparse.ArgumentParser(description=help)
-    parser.add_argument("repo", type=str, nargs="?", default="sentry")
-    args = parser.parse_args(argv)
-
-    if args.repo == "ops":
-        _fetch(coderoot, "getsentry/ops")
-        _fetch(coderoot, "getsentry/terraform-modules", sync=False)
-
-        print(
-            f"""
-    All done! Please close this terminal window and start a fresh one.
-
-    ops repo is at: {coderoot}/ops
-    """
-        )
-    elif args.repo == "sentry":
-        # git@ clones forces the use of cloning through SSH which is what we want,
-        # though CI must clone open source repos via https (no git authentication)
-        _fetch(coderoot, "getsentry/sentry", auth=CI is None, sync=False)
-
-        print("Installing sentry's brew dependencies...")
-        if CI:
-            if DARWIN:
-                # Installing everything from brew takes too much time,
-                # and chromedriver cask flakes occasionally. Really all we need to
-                # set up the devenv is colima and docker-cli.
-                # This is also required for arm64 macOS GHA runners.
-                # We manage colima, so just need to install docker + qemu here.
-                proc.run(("brew", "install", "docker", "qemu"))
-        else:
-            proc.run(
-                (f"{homebrew_bin}/brew", "bundle"), cwd=f"{coderoot}/sentry"
-            )
-
-        proc.run(
-            (sys.executable, "-P", "-m", "devenv", "sync"),
-            cwd=f"{coderoot}/sentry",
-        )
-
-        if not CI and not EXTERNAL_CONTRIBUTOR:
-            _fetch(coderoot, "getsentry/getsentry")
-
-        print(
-            f"""
-    All done! Please close this terminal window and start a fresh one.
-
-    Sentry has been set up in {coderoot}/sentry. cd into it and you should
-    be able to run `sentry devserver`.
-    """
-        )
-
-    else:
-        _fetch(coderoot, args.repo)
-
-    return 0
