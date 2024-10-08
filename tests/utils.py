b5a1fb17d250109182015cd4b2b60eb85a4b10f8
Joshua Li <joshuarli98@gmail.com>
2024-10-08 15:57:15 -0700

    sorted_walk

--- tests/utils.py
+++ tests/utils.py
@@ -5,8 +5,27 @@ import pathlib
 from collections.abc import Iterator
 
 
+# slightly modded from os.walk
 def sorted_walk(
-    path: pathlib.Path,
+    top: pathlib.Path, topdown: bool = True, followlinks: bool = False
 ) -> Iterator[tuple[str, list[str], list[str]]]:
-    for dirpath, dirnames, filenames in os.walk(path):
-        yield dirpath, sorted(dirnames), sorted(filenames)
+    names = os.listdir(top)
+
+    dirs: list[str] = []
+    nondirs: list[str] = []
+
+    for name in names:
+        if os.path.isdir(os.path.join(top, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+
+    if topdown:
+        yield f"{top}", sorted(dirs), sorted(nondirs)
+    for name in dirs:
+        new_path = top / name
+        if followlinks or not os.path.islink(new_path):
+            for x in sorted_walk(new_path, topdown, followlinks):
+                yield x
+    if not topdown:
+        yield f"{top}", sorted(dirs), sorted(nondirs)
