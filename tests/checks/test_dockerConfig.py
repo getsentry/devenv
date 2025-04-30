e25a6d635cb9779f9113aba39603ffff7d4b9e4a
Joshua Li <joshuarli98@gmail.com>
2025-04-30 12:54:01 -0700

    pop docker config cliPluginsExtraDirs

--- tests/checks/test_dockerConfig.py
+++ tests/checks/test_dockerConfig.py
@@ -8,7 +8,7 @@ from unittest import mock
 
 import pytest
 
-from devenv.checks import credsStore
+from devenv.checks import dockerConfig
 
 
 @pytest.fixture
@@ -25,26 +25,32 @@ def fake_config(tmp_path: pathlib.Path) -> Generator[pathlib.Path]:
 
 def test_no_credsStore_ok(fake_config: pathlib.Path) -> None:
     fake_config.write_text("{}")
-    assert credsStore.check() == (True, "")
+    assert dockerConfig.check() == (True, "")
 
 
 def test_binary_ok(fake_config: pathlib.Path) -> None:
     fake_config.write_text('{"credsStore": "example"}')
     with mock.patch.object(shutil, "which", return_value="/fake/exe"):
-        assert credsStore.check() == (True, "")
+        assert dockerConfig.check() == (True, "")
 
 
 @pytest.mark.parametrize("name", ("desktop", "osxkeychain"))
 def test_binary_missing(fake_config: pathlib.Path, name: str) -> None:
     fake_config.write_text(f'{{"credsStore": "{name}"}}')
     with mock.patch.object(shutil, "which", return_value=None):
-        assert credsStore.check() == (
+        assert dockerConfig.check() == (
             False,
             "credsStore requires nonexistent binary",
         )
 
 
-def test_fix(fake_config: pathlib.Path) -> None:
+def test_fix_credsStore(fake_config: pathlib.Path) -> None:
     fake_config.write_text('{"credsStore": "bad"}')
-    assert credsStore.fix() == (True, "")
+    assert dockerConfig.fix() == (True, "")
+    assert fake_config.read_text() == "{}"
+
+
+def test_fix_cliPluginsExtraDirs(fake_config: pathlib.Path) -> None:
+    fake_config.write_text('{"cliPluginsExtraDirs": ["foo/"]}')
+    assert dockerConfig.fix() == (True, "")
     assert fake_config.read_text() == "{}"
