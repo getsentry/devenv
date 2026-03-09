61b276dbd868307edc916640b93e7d209f8656f9
joshuarli <joshuarli98@gmail.com>
2026-03-09 13:49:29 -0700

    ref: deprecate gcloud (#224)

--- devenv/lib/gcloud.py
+++ devenv/lib/gcloud.py
@@ -68,6 +68,10 @@ def uninstall(binroot: str) -> None:
 
 
 def install(version: str, url: str, sha256: str, reporoot: str) -> None:
+    print(
+        "!!! devenv-managed gcloud is deprecated! run `brew install --cask gcloud-cli` !!!"
+    )
+
     binroot = fs.ensure_binroot(reporoot)
 
     if (
